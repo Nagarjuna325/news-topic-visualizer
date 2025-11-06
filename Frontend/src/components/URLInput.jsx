1117f9b24d06a1946555ace2bc279491a514f04a
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2025-11-06 15:04:03 -0600

    Changes made to frontend file URLinput

--- Frontend/src/components/URLInput.jsx
+++ Frontend/src/components/URLInput.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 const SAMPLE_URLS = [
   'https://www.bbc.com/news/technology',
-  'https://www.theguardian.com/us/technology',
   'https://techcrunch.com/',
   'https://www.wired.com/',
   'https://arstechnica.com/'
